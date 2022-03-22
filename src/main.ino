de1cd950745f2e0eb32ce68df3a8a5ce6f8dd50b
ykrismmmd <54070669+ykrismmmd@users.noreply.github.com>
2022-03-22 19:14:23 +0900

    新しいブランチ

--- src/main.ino
+++ src/main.ino
@@ -1,23 +1,3 @@
-/***************************************************
-  DFPlayer - A Mini MP3 Player For Arduino
-  <https://www.dfrobot.com/product-1121.html>
-
- ***************************************************
-  This example shows the basic function of library for DFPlayer.
-
-  Created 2016-12-07
-  By [Angelo qiao](Angelo.qiao@dfrobot.com)
-
-  GNU Lesser General Public License.
-  See <http://www.gnu.org/licenses/> for details.
-  All above must be included in any redistribution
- ****************************************************/
-
-/***********Notice and Trouble shooting***************
-  1.Connection and Diagram can be found here
-  <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
-  2.This code is tested on Arduino Uno, Leonardo, Mega boards.
- ****************************************************/
 #include "stdlib.h" //一般ユーティリティに関する型、マクロ、関数宣言、定義ライブラリ読み込み
 #include "Arduino.h" //esp32だけどarduinoのライブラリ活用
 //#include "SoftwareSerial.h"
